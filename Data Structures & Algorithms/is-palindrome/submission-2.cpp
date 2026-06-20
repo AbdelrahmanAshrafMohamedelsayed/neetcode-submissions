e861aab19047fb3abf4c180fc1ca6c3741d5902b
Abdelrahman Ashraf <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2026-06-20 21:46:21 +0300

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string s2="";
+        for( char c:s){
+            if(isalpha(c) || isdigit(c)){
+                s2+=tolower(c);
+            }
+        }
+    int l= 0;
+    int r=s2.size()-1;
+    cout<<s2;
+    while(l<r){
+        if(s2[l]!=s2[r]){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+    }
+};
