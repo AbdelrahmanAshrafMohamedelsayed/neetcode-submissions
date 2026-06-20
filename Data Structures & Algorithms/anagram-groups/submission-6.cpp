f03e5d87ba61382ba41af05feda1b268bc5c8f5e
Abdelrahman Ashraf <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2026-06-20 21:08:35 +0300

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> s;
+        map<vector<int>, vector<string>>mp;
+        for( string s: strs){
+            string sort_num=s;
+            // calc the freq array
+            vector<int>freq(26);
+            for(char c:s){
+                freq[c-'a']++;
+            }
+            mp[freq].push_back(s);
+        }
+        for(auto it: mp){
+            s.push_back(it.second);
+        }
+        return s;
+    }
+};
