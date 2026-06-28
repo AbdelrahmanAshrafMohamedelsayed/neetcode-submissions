427065e471c16922003c67d29dca65bcf3129b49
Abdelrahman Ashraf <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2026-06-28 10:48:19 +0300

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int k= s1.size();
+        int l=0;
+        map<char,int>mp1;
+        map<char,int>mp2;
+        if(s1.size()>s2.size()){
+            return false;
+        }
+        for(int i=0;i<k; i++){
+             mp1[s1[i]]++;
+             mp2[s2[i]]++;
+        }
+        if(mp1==mp2){
+            return true;
+        }
+        for(int r=k;r<s2.size(); r++){
+            mp2[s2[r]]++;
+            mp2[s2[r-k]]--;
+            if(mp2[s2[r-k]] == 0)
+                mp2.erase(s2[r-k]);
+            if(mp1==mp2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
