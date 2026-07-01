f3886058f6ef602e73ae420ddabffbcc99456601
Abdelrahman Ashraf <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2026-07-01 14:07:46 +0300

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n= position.size();
+        vector<pair<int,int>>cars(n);
+        vector<double>times;
+        stack<double>s;
+        for(int i=0; i<n; i++){
+            cars[i]={position[i],speed[i]};
+        }
+        sort(cars.begin(),cars.end());
+        
+        for(int i=0; i<n; i++){
+     
+            times.push_back((double)(target-cars[i].first)/cars[i].second);
+        }
+        for(int i=n-1; i>=0; i--){
+            if (s.empty() || times[i] > s.top()) {
+                s.push(times[i]);
+            }
+
+        }
+        return s.size();
+    }
+};
